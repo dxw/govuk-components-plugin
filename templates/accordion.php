48d61380e45bfcabd6dbd7776e56b8dc14d3a07c
Rob Skilling <rob@dxw.com>
2021-01-20 16:00:40 +0000

    Add filters for amending the default govuk class prefixes
    Nothing hooks into this filter yet, and exact implementation is TBC.
    
    But this will let us supplement/override the default govuk- prefixed classes with provider specific ones, so that extra styling can be applied.
    
    Resolves: https://trello.com/c/DQYThN4e/17-add-accordion-component-into-plugin

--- templates/accordion.php
+++ templates/accordion.php
@@ -5,20 +5,20 @@ $rowCount = 0;
 
 ?>
 
-<div class="govuk-accordion" data-module="govuk-accordion" id="<?php echo $accordionId ?>">
+<div class="<?php echo apply_filters('govuk_components_class', 'govuk-accordion') ?>" data-module="govuk-accordion" id="<?php echo $accordionId ?>">
     <?php if (have_rows('accordion_sections')) :
-        while (have_rows('accordion_sections')) : the_row(); $rowCount++; 
+        while (have_rows('accordion_sections')) : the_row(); $rowCount++;
         $headingId = $accordionId . '-heading-' . $rowCount;
         ?>
-        <div class="govuk-accordion__section">
-            <div class="govuk-accordion__section-header">
-                <h2 class="govuk-accordion__section-heading">
-                    <span class="govuk-accordion__section-button" id="<?php echo $headingId ?>">
+        <div class="<?php echo apply_filters('govuk_components_class', 'govuk-accordion__section') ?>">
+            <div class="<?php echo apply_filters('govuk_components_class', 'govuk-accordion__section-header') ?>">
+                <h2 class="<?php echo apply_filters('govuk_components_class', 'govuk-accordion__section-heading') ?>">
+                    <span class="<?php echo apply_filters('govuk_components_class', 'govuk-accordion__section-button') ?>" id="<?php echo $headingId ?>">
                     <?php the_sub_field('accordion_section_heading'); ?>
                     </span>
                 </h2>
             </div>
-            <div id="<?php echo $accordionId ?>-content-<?php echo $rowCount ?>" class="govuk-accordion__section-content" aria-labelledby="<?php echo $headingId ?>">
+            <div id="<?php echo $accordionId ?>-content-<?php echo $rowCount ?>" class="<?php echo apply_filters('govuk_components_class', 'govuk-accordion__section-content') ?>" aria-labelledby="<?php echo $headingId ?>">
                 <?php the_sub_field('accordion_section_content'); ?>
             </div>
         </div>
