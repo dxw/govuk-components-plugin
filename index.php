1b2c9984039c04e6f022e5a70b9cefcae832ceac
Rob Skilling <rjs.mod@googlemail.com>
2021-01-20 11:07:32 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: My Plugin
+Description: TODO
+Version: 0.1.0
+Author: dxw
+Author URI: https://www.dxw.com
+*/
+
+$registrar = require __DIR__.'/app/load.php';
+$registrar->register();
