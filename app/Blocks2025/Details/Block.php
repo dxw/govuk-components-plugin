80a7df03dd19353aa661c627be7edc89c0863e9f
william-man <william.man@dxw.com>
2025-07-01 14:57:56 +0100

    Register callback to register Details Block

--- app/Blocks2025/Details/Block.php
+++ app/Blocks2025/Details/Block.php
@@ -6,6 +6,6 @@ class Block implements \Dxw\Iguana\Registerable
 {
 	public function register()
 	{
-
+		add_action('init', [$this, 'registerBlock'], 10, 0);
 	}
 }
--- spec/blocks2025/details/block.spec.php
+++ spec/blocks2025/details/block.spec.php
@@ -8,4 +8,14 @@ describe(\GovukComponents\Blocks2025\Details\Block::class, function () {
 	it('implements the register interface', function () {
 		expect($this->block)->toBeAnInstanceOf(\Dxw\Iguana\Registerable::class);
 	});
+
+	describe('->register()', function () {
+		it('it registers the callback to register the block', function () {
+			allow('add_action')->toBeCalled();
+
+			expect('add_action')->toBeCalled()->once()->with('init', [$this->block, 'registerBlock'], 10, 0);
+
+			$this->block->register();
+		});
+	});
 });
