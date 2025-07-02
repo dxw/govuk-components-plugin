638ab57719235b06f247c06b3a9b81d3af439dd0
william-man <william.man@dxw.com>
2025-07-02 12:04:27 +0100

    New Block class implements iBlock interface

--- spec/blocks/details/block.spec.php
+++ spec/blocks/details/block.spec.php
@@ -4,4 +4,8 @@ describe(\GovukComponents\Blocks\Details\Block::class, function () {
 	beforeEach(function () {
 		$this->block = new GovukComponents\Blocks\Details\Block();
 	});
+
+	it('implements the iBlock interface', function () {
+		expect($this->block)->toBeAnInstanceOf(GovukComponents\Blocks\iBlock::class);
+	});
 });
