0838cc07668297480fed760e46acd7e7fe7065ba
william-man <william.man@dxw.com>
2025-07-04 15:17:36 +0100

    Accordion Block implements the iBlock interface

--- spec/blocks/accordion/block.spec.php
+++ spec/blocks/accordion/block.spec.php
@@ -4,4 +4,8 @@ describe(\GovukComponents\Blocks\Accordion\Block::class, function () {
 	beforeEach(function () {
 		$this->block = new GovukComponents\Blocks\Accordion\Block();
 	});
+
+	it('implements the iBlock interface', function () {
+		expect($this->block)->toBeAnInstanceOf(\GovukComponents\Blocks\iBlock::class);
+	});
 });
