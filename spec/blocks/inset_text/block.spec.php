215494f064b13b3241303c256b7218b437dc0bf2
william-man <william.man@dxw.com>
2025-07-04 11:26:41 +0100

    InsetText block implements iBlock interface

--- spec/blocks/inset_text/block.spec.php
+++ spec/blocks/inset_text/block.spec.php
@@ -4,4 +4,8 @@ describe(\GovukComponents\Blocks\InsetText\Block::class, function () {
 	beforeEach(function () {
 		$this->block = new GovukComponents\Blocks\InsetText\Block();
 	});
+
+	it('implements the iBlock interface', function () {
+		expect($this->block)->toBeAnInstanceOf(\GovukComponents\Blocks\iBlock::class);
+	});
 });
