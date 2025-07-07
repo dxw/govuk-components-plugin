ba5a92cc2bddf4882ca25e2520e212376591ae49
william-man <william.man@dxw.com>
2025-07-07 14:45:14 +0100

    Tabs Block implements the iBlock interface

--- spec/blocks/tabs/block.spec.php
+++ spec/blocks/tabs/block.spec.php
@@ -4,4 +4,8 @@ describe(\GovukComponents\Blocks\Tabs\Block::class, function () {
 	beforeEach(function () {
 		$this->block = new GovukComponents\Blocks\Tabs\Block();
 	});
+
+	it('implements the iBlock interface', function () {
+		expect($this->block)->toBeAnInstanceOf(\GovukComponents\Blocks\iBlock::class);
+	});
 });
