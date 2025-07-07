d352be0f7c3b52cc9acbf1eb6e24d7e4bff97e47
william-man <william.man@dxw.com>
2025-07-07 15:14:58 +0100

    Create new Warning Text Block class

---
+++ app/Blocks/WarningText/Block.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace GovukComponents\Blocks\WarningText;
+
+class Block
+{
+}
---
+++ spec/blocks/warning_text/block.spec.php
@@ -0,0 +1,7 @@
+<?php
+
+describe(\GovukComponents\Blocks\WarningText\Block::class, function () {
+	beforeEach(function () {
+		$this->block = new GovukComponents\Blocks\WarningText\Block();
+	});
+});
