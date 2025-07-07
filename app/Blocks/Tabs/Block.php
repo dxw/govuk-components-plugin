3b3200d1a018846e3fdc27c15f597e9339a2d23b
william-man <william.man@dxw.com>
2025-07-07 14:48:22 +0100

    Register action to register Tabs Block

--- app/Blocks/Tabs/Block.php
+++ app/Blocks/Tabs/Block.php
@@ -6,7 +6,7 @@ class Block implements \GovukComponents\Blocks\iBlock
 {
 	public function init(): void
 	{
-
+		add_action('init', [$this, 'registerBlock']);
 	}
 
 	public function getDisplayName(): string
