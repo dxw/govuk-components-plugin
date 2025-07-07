bb61ee8e46df5fab7929522aa33e85db7523d9ae
william-man <william.man@dxw.com>
2025-07-07 15:30:07 +0100

    Register action to register Warning Text Block

--- spec/blocks/warning_text/block.spec.php
+++ spec/blocks/warning_text/block.spec.php
@@ -8,4 +8,14 @@ describe(\GovukComponents\Blocks\WarningText\Block::class, function () {
 	it('implements the iBlock interface', function () {
 		expect($this->block)->toBeAnInstanceOf(GovukComponents\Blocks\iBlock::class);
 	});
+
+	describe('->init()', function () {
+		it('registers actions to register the block', function () {
+			allow('add_action')->toBeCalled();
+
+			expect('add_action')->toBeCalled()->once()->with('init', [$this->block, 'registerBlock']);
+
+			$this->block->init();
+		});
+	});
 });
