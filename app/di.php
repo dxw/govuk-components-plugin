46828c3175df09323e619f4f59ee5eb9272e381f
william-man <william.man@dxw.com>
2025-07-07 12:36:18 +0100

    Update di file with new InsetText Block

--- app/di.php
+++ app/di.php
@@ -4,7 +4,7 @@ $registrar->addInstance(new \GovukComponents\Blocks\Accordion\Block());
 $registrar->addInstance(new \GovukComponents\Blocks\AccordionRow\Block());
 $registrar->addInstance(new \GovukComponents\Blocks\Button());
 $registrar->addInstance(new \GovukComponents\Blocks\Details\Block());
-$registrar->addInstance(new \GovukComponents\Blocks\InsetText());
+$registrar->addInstance(new \GovukComponents\Blocks\InsetText\Block());
 $registrar->addInstance(new \GovukComponents\Blocks\NotificationBanner());
 $registrar->addInstance(new \GovukComponents\Blocks\WarningText());
 
@@ -16,7 +16,7 @@ $registrar->addInstance(new \GovukComponents\BlockController(
 		$registrar->getInstance(\GovukComponents\Blocks\AccordionRow\Block::class),
 		$registrar->getInstance(\GovukComponents\Blocks\Button::class),
 		$registrar->getInstance(\GovukComponents\Blocks\Details\Block::class),
-		$registrar->getInstance(\GovukComponents\Blocks\InsetText::class),
+		$registrar->getInstance(\GovukComponents\Blocks\InsetText\Block::class),
 		$registrar->getInstance(\GovukComponents\Blocks\NotificationBanner::class),
 		$registrar->getInstance(\GovukComponents\Blocks\WarningText::class)
 	]
