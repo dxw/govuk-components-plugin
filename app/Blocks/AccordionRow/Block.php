a6b299ff3d4b0b69477c6f7bbee5971bd4386522
william-man <william.man@dxw.com>
2025-07-07 10:34:51 +0100

    AccordionRow Block implements the iBlock interface

--- app/Blocks/AccordionRow/Block.php
+++ app/Blocks/AccordionRow/Block.php
@@ -2,6 +2,20 @@
 
 namespace GovukComponents\Blocks\AccordionRow;
 
-class Block
+class Block implements \GovukComponents\Blocks\iBlock
 {
+	public function init(): void
+	{
+
+	}
+
+	public function getDisplayName(): string
+	{
+
+	}
+
+	public function getOptionName(): string
+	{
+
+	}
 }
--- spec/blocks/accordion_row.spec.php/block.spec.php
+++ spec/blocks/accordion_row.spec.php/block.spec.php
@@ -4,4 +4,8 @@ describe(\GovukComponents\Blocks\AccordionRow\Block::class, function () {
 	beforeEach(function () {
 		$this->block = new GovukComponents\Blocks\AccordionRow\Block();
 	});
+
+	it('implements the iBlock interface', function () {
+		expect($this->block)->toBeAnInstanceOf(\GovukComponents\Blocks\iBlock::class);
+	});
 });
