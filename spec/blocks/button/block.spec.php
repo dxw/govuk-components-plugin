ad4cd8c3456c97ed422bb364d3d12f89ff57deb7
william-man <william.man@dxw.com>
2025-07-08 14:19:28 +0100

    Button Block implements the iBlock interface

--- spec/blocks/button/block.spec.php
+++ spec/blocks/button/block.spec.php
@@ -4,4 +4,8 @@ describe(\GovukComponents\Blocks\Button\Block::class, function () {
 	beforeEach(function () {
 		$this->block = new GovukComponents\Blocks\Button\Block();
 	});
+
+	it('implements the iBlock interface', function () {
+		expect($this->block)->toBeAnInstanceOf(\GovukComponents\Blocks\iBlock::class);
+	});
 });
