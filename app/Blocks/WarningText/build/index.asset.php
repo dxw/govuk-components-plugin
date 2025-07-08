b6000f8ee45b879c3fbb3b47f98ae7b53b4bc2ff
william-man <william.man@dxw.com>
2025-07-08 11:04:40 +0100

    Fix php-cs-fixer lint error

--- app/Blocks/WarningText/build/index.asset.php
+++ app/Blocks/WarningText/build/index.asset.php
@@ -1,1 +1,3 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '8fbe1954d220c600e549');
+<?php
+
+return ['dependencies' => ['react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'], 'version' => '8fbe1954d220c600e549'];
