62c558b33243dcf16f670eaebfe7e37e296834cd
william-man <william.man@dxw.com>
2025-07-09 17:48:14 +0100

    Register action to enqueue block variations

--- app/Scripts.php
+++ app/Scripts.php
@@ -6,6 +6,6 @@ class Scripts implements \Dxw\Iguana\Registerable
 {
 	public function register(): void
 	{
-
+		add_action('enqueue_block_editor_assets', [$this, 'govukComponentsBlockVariations']);
 	}
 }
--- spec/scripts.spec.php
+++ spec/scripts.spec.php
@@ -8,4 +8,14 @@ describe(GovukComponents\Scripts::class, function () {
 	it('implements the Registerable interface', function () {
 		expect($this->scripts)->toBeAnInstanceOf(\Dxw\Iguana\Registerable::class);
 	});
+
+	describe('->register()', function () {
+		it('adds action', function () {
+			allow('add_action')->toBeCalled();
+
+			expect('add_action')->toBeCalled()->once()->with('enqueue_block_editor_assets', [$this->scripts, 'govukComponentsBlockVariations']);
+
+			$this->scripts->register();
+		});
+	});
 });
