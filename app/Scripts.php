120b762b1c6226c5847241369749ebc707e26d11
william-man <william.man@dxw.com>
2025-07-10 14:29:21 +0100

    Enqueue script to register block style variations

--- app/Scripts.php
+++ app/Scripts.php
@@ -6,6 +6,19 @@ class Scripts implements \Dxw\Iguana\Registerable
 {
 	public function register(): void
 	{
-		add_action('enqueue_block_editor_assets', [$this, 'govukComponentsBlockStyleVariations']);
+		add_action('enqueue_block_editor_assets', [$this, 'blockVariations']);
+	}
+
+	public function blockVariations(): void
+	{
+		wp_enqueue_script(
+			'block-style-variations',
+			plugin_dir_url(__DIR__) . 'assets/js/block-variations.js',
+			[
+				'wp-blocks',
+				'wp-dom-ready',
+				'wp-edit-post'
+			]
+		);
 	}
 }
--- spec/scripts.spec.php
+++ spec/scripts.spec.php
@@ -1,5 +1,7 @@
 <?php
 
+use Kahlan\Arg;
+
 describe(\GovukComponents\Scripts::class, function () {
 	beforeEach(function () {
 		$this->scripts = new GovukComponents\Scripts();
@@ -14,9 +16,21 @@ describe(\GovukComponents\Scripts::class, function () {
 			allow('add_action')->toBeCalled();
 
 			expect('add_action')->toBeCalled()->once()
-			->with('enqueue_block_editor_assets', [$this->scripts, 'govukComponentsBlockStyleVariations']);
+			->with('enqueue_block_editor_assets', [$this->scripts, 'blockVariations']);
 
 			$this->scripts->register();
+		});
+	});
+
+	describe('->blockStyleVariations()', function () {
+		it('enqueues block style variations script', function () {
+			allow('wp_enqueue_script')->toBeCalled();
+			allow('plugin_dir_url')->toBeCalled();
+
+			expect('wp_enqueue_script')->toBeCalled()->once()
+			->with('block-style-variations', Arg::toBeA('string'), Arg::toBeAn('array'));
+
+			$this->scripts->blockVariations();
 		});
 	});
 });
