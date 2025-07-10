4512dd3f12f9f48d6814fba465eae090fd21af84
william-man <william.man@dxw.com>
2025-07-10 14:08:34 +0100

    Register action to enqueue block style variations

--- spec/scripts.spec.php
+++ spec/scripts.spec.php
@@ -8,4 +8,15 @@ describe(\GovukComponents\Scripts::class, function () {
 	it('implements the Registerable interface', function () {
 		expect($this->scripts)->toBeAnInstanceOf(\Dxw\Iguana\Registerable::class);
 	});
+
+	describe('->register()', function () {
+		it('adds the actions', function () {
+			allow('add_action')->toBeCalled();
+
+			expect('add_action')->toBeCalled()->once()
+			->with('enqueue_block_editor_assets', [$this->scripts, 'govukComponentsBlockStyleVariations']);
+
+			$this->scripts->register();
+		});
+	});
 });
