ab6a5e642211bc70ac34e53077fbec6c9be2eb63
william-man <william.man@dxw.com>
2025-07-15 14:10:40 +0100

    Compile src files into build directory

--- app/Blocks/WarningText/build/index.asset.php
+++ app/Blocks/WarningText/build/index.asset.php
@@ -1,3 +1,3 @@
 <?php
 
-return ['dependencies' => ['react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'], 'version' => '2c6b3f6cac8424120e09'];
+return ['dependencies' => ['react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'], 'version' => '8b4729e9397459b641b8'];
