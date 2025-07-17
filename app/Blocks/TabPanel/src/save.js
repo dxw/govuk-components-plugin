1f7d7de47afd7eefee640f7c01600b84890f97a8
william-man <william.man@dxw.com>
2025-07-17 16:02:27 +0100

    Insert markup structure for saving Tab Panel
    Saves the block's title as a heading and an id generated from the title will
    be used for linking. Content inserted into the block will render under the
    heading eleent.

--- app/Blocks/TabPanel/src/save.js
+++ app/Blocks/TabPanel/src/save.js
@@ -1,10 +1,23 @@
-import { useBlockProps } from '@wordpress/block-editor';
+import { RichText, useBlockProps, useInnerBlocksProps } from '@wordpress/block-editor';
 
-export default function Save() {
-    const blockProps = useBlockProps();
+export default function Save({ attributes }) {
+    const { title, panelId } = attributes
+
+    const blockProps = useBlockProps.save(
+        {
+            className:'govuk-tabs__panel'
+        }
+    );
+
+    const { children, ...innerBlocksProps } = useInnerBlocksProps.save(blockProps);
+    
     
     return (
-        <div {...blockProps}>
+        <div { ...innerBlocksProps } id={ panelId }>
+            <h2 className='govuk-heading-l'>
+                <RichText.Content value={ title } />
+            </h2>
+            { children }
         </div>
     );
 }
