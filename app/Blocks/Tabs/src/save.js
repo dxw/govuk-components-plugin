75f6cfbcd0698e1bc34209c1234a38fa985222b7
william-man <william.man@dxw.com>
2025-07-17 16:27:08 +0100

    Add save markup for Tabs block with tab list and content panels
    Renders the tab list using the stored titles from the 'tabs' attribute.
    Also outputs saved panel content as children of this block.

--- app/Blocks/Tabs/src/save.js
+++ app/Blocks/Tabs/src/save.js
@@ -1,10 +1,33 @@
-import { useBlockProps } from '@wordpress/block-editor';
+import { useBlockProps, useInnerBlocksProps } from '@wordpress/block-editor';
 
-export default function Save() {
-    const blockProps = useBlockProps();
-    
+export default function Save({ attributes }) {
+    const { tabs } = attributes;
+
+    const blockProps = useBlockProps.save(
+        {
+		    className: 'govuk-tabs',
+            "data-module": 'govuk-tabs'
+        }
+    );
+    const { children, ...innerBlocksProps } = useInnerBlocksProps.save(blockProps);
+
     return (
-        <div {...blockProps}>
-        </div>
+            <div { ...innerBlocksProps }>
+                <h2 className="govuk-tabs__title">
+					Content
+				</h2>
+				<ul className="govuk-tabs__list">
+                    {tabs.map((tab) => {
+                        return (
+                            <li className='govuk-tabs__list-item'>
+                                <a className='govuk-tabs__tab' href={ '#' + tab.id }>
+                                    { tab.title }
+                                </a>
+                            </li>
+                        )
+                    })}
+				</ul>
+                {children}
+            </div>
     );
 }
