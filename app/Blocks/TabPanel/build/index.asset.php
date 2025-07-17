230ed3e339f2099066fc95a7d7657d1be31629f9
william-man <william.man@dxw.com>
2025-07-17 16:22:37 +0100

    Compile src files into build directory

--- app/Blocks/TabPanel/build/index.asset.php
+++ app/Blocks/TabPanel/build/index.asset.php
@@ -1,3 +1,3 @@
 <?php
 
-return ['dependencies' => ['react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'], 'version' => 'a0061e1cc0e24fb1ab92'];
+return ['dependencies' => ['react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'], 'version' => '62042161c79ede782f16'];
