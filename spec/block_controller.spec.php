29ac37ee2bf649b1f2591244a4ac8649e75f98bf
william-man <william.man@dxw.com>
2025-07-25 17:05:10 +0100

    Add a tighter check for decoding the json data
    This replaces the annotation for $data and addsa guard to return
    false if there are any errors decoding the json data.

--- spec/block_controller.spec.php
+++ spec/block_controller.spec.php
@@ -108,6 +108,13 @@ describe(\GovukComponents\BlockController::class, function () {
 				expect($this->blockController->hasParent('Custom Block'))->toBe(false);
 			});
 
+			it('returns false if the data cannot be decoded or there is an error', function () {
+				allow('file_get_contents')->toBeCalled()->andReturn('{}');
+				allow('json_decode')->toBeCalled()->andReturn(null);
+
+				expect($this->blockController->hasParent('Custom Block'))->toBe(false);
+			});
+
 			it('returns true if the block has a parent', function () {
 				allow('file_get_contents')->toBeCalled()->andReturn('{
 					"parent": [ "parent-block" ]
@@ -115,8 +122,6 @@ describe(\GovukComponents\BlockController::class, function () {
 
 				expect($this->blockController->hasParent('Custom Block'))->toBe(true);
 			});
-
-
 
 			it('returns false if the block does not have a parent', function () {
 				allow('file_get_contents')->toBeCalled()->andReturn('{}');
