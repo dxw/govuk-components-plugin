e359c2d721e446848ed881d09e975ac791888314
Sarah Mount <sarah.mount@dxw.com>
2025-09-01 10:03:00 +0100

    Render the GovUK phase banner with a feedback URL

--- app/Components/PhaseBanner.php
+++ app/Components/PhaseBanner.php
@@ -13,5 +13,25 @@ final class PhaseBanner implements \Dxw\Iguana\Registerable
 
 	public function displayPhaseBanner(): void
 	{
+		/** @var string $phase */
+		$phase = get_field('govuk_components_phase_banner_phase', 'option');
+		/** @var string $feedbackUrl */
+		$feedbackUrl = get_field('govuk_components_phase_banner_feedback_url', 'option');
+
+		if ($phase === 'off' || empty($phase)) {
+			return;
+		}
+
+		$phaseText = strtoupper(trim($phase));
+		$message = 'This is a new service - your <a class="govuk-link" href="' . esc_url($feedbackUrl) . '">feedback</a> will help us to improve it.';
+
+		?>
+<div class="govuk-phase-banner">
+    <p class="govuk-phase-banner__content">
+        <strong class="govuk-tag govuk-phase-banner__content__tag"><?= esc_html($phaseText); ?></strong>
+        <span class="govuk-phase-banner__text"><?= $message; ?></span>
+    </p>
+</div>
+<?php
 	}
 }
--- spec/components/phase_banner.spec.php
+++ spec/components/phase_banner.spec.php
@@ -18,4 +18,48 @@ describe(\GovukComponents\Components\PhaseBanner::class, function () {
 		});
 	});
 
+	describe('->displayPhaseBanner)', function () {
+		beforeEach(function () {
+			allow('esc_html')->toBeCalled()->andRun(function ($val) {
+				return $val;
+			});
+			allow('esc_url')->toBeCalled()->andRun(function ($val) {
+				return $val;
+			});
+		});
+		context('The phase banner is switched off in options', function () {
+			it('does nothing', function () {
+				allow('get_field')->toBecalled()->andReturn(
+					'off',
+					''
+				);
+				$expected = '';
+				ob_start();
+				$this->banner->displayPhaseBanner();
+				$result = ob_get_clean();
+				expect($result)->toEqual($expected);
+			});
+		});
+		context('The phase banner is switched on in options', function () {
+			it('renders the phase banner', function () {
+				allow('get_field')->toBecalled()->andReturn(
+					'alpha',
+					'https://example.com'
+				);
+				$expected = <<<HTML
+<div class="govuk-phase-banner">
+    <p class="govuk-phase-banner__content">
+        <strong class="govuk-tag govuk-phase-banner__content__tag">ALPHA</strong>
+        <span class="govuk-phase-banner__text">This is a new service - your <a class="govuk-link" href="https://example.com">feedback</a> will help us to improve it.</span>
+    </p>
+</div>
+
+HTML;
+				ob_start();
+				$this->banner->displayPhaseBanner();
+				$result = ob_get_clean();
+				expect($result)->toEqual($expected);
+			});
+		});
+	});
 });
