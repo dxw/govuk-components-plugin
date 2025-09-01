1cefb48de6014695d0cd56cd2e61a1a5150c8454
Sarah Mount <sarah.mount@dxw.com>
2025-09-01 10:01:00 +0100

    Add callback to display GovUK phase banner
    Note that we're using a Flatpack specific hook here,
    which makes it more difficult for non-dxw code to use
    this plugin.
    
    For now, this is a pragmatic choice because we need a
    hook that exists in the parent theme, but it might be
    wise to add a hook for wp_body_open or similar.

--- app/Components/PhaseBanner.php
+++ app/Components/PhaseBanner.php
@@ -7,5 +7,11 @@ final class PhaseBanner implements \Dxw\Iguana\Registerable
 	#[\Override]
 	public function register(): void
 	{
+		/** @psalm-suppress HookNotFound */
+		add_action('dxw_flatpack_before_header', [$this, 'displayPhaseBanner'], 10, 0);
+	}
+
+	public function displayPhaseBanner(): void
+	{
 	}
 }
--- spec/components/phase_banner.spec.php
+++ spec/components/phase_banner.spec.php
@@ -8,4 +8,14 @@ describe(\GovukComponents\Components\PhaseBanner::class, function () {
 	it('is registerable', function () {
 		expect($this->banner)->toBeAnInstanceOf(\Dxw\Iguana\Registerable::class);
 	});
+
+	describe('->register()', function () {
+		it('adds the actions', function () {
+			allow('add_action')->toBeCalled();
+			expect('add_action')->toBeCalled()->once();
+			expect('add_action')->toBeCalled()->with('dxw_flatpack_before_header', [$this->banner, 'displayPhaseBanner']);
+			$this->banner->register();
+		});
+	});
+
 });
