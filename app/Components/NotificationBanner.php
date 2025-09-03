d682bed9ba04187ef11588233c4bb93834ec295f
Sarah Mount <sarah.mount@dxw.com>
2025-09-03 15:33:32 +0100

    Ensure Notification Banner appears below Phase Banner

--- app/Components/NotificationBanner.php
+++ app/Components/NotificationBanner.php
@@ -8,8 +8,8 @@ final class NotificationBanner implements \Dxw\Iguana\Registerable
 	public function register(): void
 	{
 		/** @psalm-suppress HookNotFound */
-		add_action('dxw_flatpack_before_header', [$this, 'displayNotificationBanner'], 10, 0);
-		add_action('wp_body_open', [$this, 'displayNotificationBanner'], 10, 0);
+		add_action('dxw_flatpack_before_header', [$this, 'displayNotificationBanner'], 999, 0);
+		add_action('wp_body_open', [$this, 'displayNotificationBanner'], 999, 0);
 	}
 
 	public function displayNotificationBanner(): void
