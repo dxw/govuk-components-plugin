a3cc01ca7f5ffcc6837513b1fdcbd2842cb82bcd
Sarah Mount <sarah.mount@dxw.com>
2025-09-03 15:06:43 +0100

    Implement the GOV.UK Notification Banner
    If the banner is switched off in options, do nothing.
    If it is switched on, display the output described in
    the design system documentation.
    
    Note this introduces a new hook for rendering the banner.
    
    See:
        https://design-system.service.gov.uk/components/notification-banner/

--- app/Components/NotificationBanner.php
+++ app/Components/NotificationBanner.php
@@ -8,11 +8,33 @@ final class NotificationBanner implements \Dxw\Iguana\Registerable
 	public function register(): void
 	{
 		/** @psalm-suppress HookNotFound */
-		add_action('dxw_flatpack_before_header', [$this, 'displayNotificationBanner'], 10, 0);
-		add_action('wp_body_open', [$this, 'displayNotificationBanner'], 10, 0);
+		add_action('govuk-components-notification-banner-render', [$this, 'displayNotificationBanner'], 10, 0);
 	}
+
 
 	public function displayNotificationBanner(): void
 	{
+		/** @var string $show */
+		$show = get_field('govuk_components_notification_banner_show', 'option');
+
+		if ($show === 'off') {
+			return;
+		}
+
+		/** @var string $content */
+		$content = get_field('govuk_components_notification_banner_content', 'option');
+
+		?>
+<div class="govuk-notification-banner" role="region" aria-labelledby="govuk-notification-banner-title" data-module="govuk-notification-banner">
+  <div class="govuk-notification-banner__header">
+    <h2 class="govuk-notification-banner__title" id="govuk-notification-banner-title">
+      Important
+    </h2>
+  </div>
+  <div class="govuk-notification-banner__content">
+<?= wp_kses_post($content); ?>
+  </div>
+</div>
+<?php
 	}
 }
--- spec/components/notification_banner.spec.php
+++ spec/components/notification_banner.spec.php
@@ -12,11 +12,54 @@ describe(\GovukComponents\Components\NotificationBanner::class, function () {
 	describe('->register()', function () {
 		it('adds the actions', function () {
 			allow('add_action')->toBeCalled();
-			expect('add_action')->toBeCalled()->times(2);
-			expect('add_action')->toBeCalled()->with('dxw_flatpack_before_header', [$this->banner, 'displayNotificationBanner']);
-			expect('add_action')->toBeCalled()->with('wp_body_open', [$this->banner, 'displayNotificationBanner']);
+			expect('add_action')->toBeCalled()->once();
+			expect('add_action')->toBeCalled()->with('govuk-components-notification-banner-render', [$this->banner, 'displayNotificationBanner'], 10, 0);
 			$this->banner->register();
 		});
 	});
 
+	describe('->displayNotificationBanner)', function () {
+		beforeEach(function () {
+			allow('wp_kses_post')->toBeCalled()->andRun(function ($val) {
+				return $val;
+			});
+		});
+		context('The banner is switched off in options', function () {
+			it('does nothing', function () {
+				allow('get_field')->toBecalled()->andReturn(
+					'off',
+					''
+				);
+				$expected = '';
+				ob_start();
+				$this->banner->displayNotificationBanner();
+				$result = ob_get_clean();
+				expect($result)->toEqual($expected);
+			});
+		});
+		context('The banner is switched on in options', function () {
+			it('renders the phase banner', function () {
+				allow('get_field')->toBecalled()->andReturn(
+					'on',
+					'<p><em>GODZILLA IS APPROACHING!</em></p>'
+				);
+				$expected = <<<HTML
+<div class="govuk-notification-banner" role="region" aria-labelledby="govuk-notification-banner-title" data-module="govuk-notification-banner">
+  <div class="govuk-notification-banner__header">
+    <h2 class="govuk-notification-banner__title" id="govuk-notification-banner-title">
+      Important
+    </h2>
+  </div>
+  <div class="govuk-notification-banner__content">
+<p><em>GODZILLA IS APPROACHING!</em></p>  </div>
+</div>
+
+HTML;
+				ob_start();
+				$this->banner->displayNotificationBanner();
+				$result = ob_get_clean();
+				expect($result)->toEqual($expected);
+			});
+		});
+	});
 });
