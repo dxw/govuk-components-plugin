7db7f0bf0b1d1072af93f81bde0e9dbc0815802e
Ben Atherton <ben.atherton@dxw.com>
2025-10-22 08:51:20 +0100

    Psalm and CS fixes

--- app/Blocks/Pagination/Render.php
+++ app/Blocks/Pagination/Render.php
@@ -2,17 +2,24 @@
 
 namespace GovukComponents\Blocks\Pagination;
 
-function renderPaginationLink($post, $isPrev = true)
+use WP_Post;
+
+/**
+ * @param WP_Post|null $post
+ * @param bool $isPrev
+ * @return string
+ */
+function renderPaginationLink(?WP_Post $post, bool $isPrev = true): string
 {
-	if (!$post) {
+	if ($post === null) {
 		return '';
 	}
 
 	$class = $isPrev ? 'govuk-pagination__prev' : 'govuk-pagination__next';
 	$rel = $isPrev ? 'prev' : 'next';
 	$titleText = $isPrev ? 'Previous page' : 'Next page';
-	$url = get_permalink($post);
-	$label = get_the_title($post);
+	$url = get_permalink($post) ?: '';
+	$label = get_the_title($post) ?: '';
 
 	$svg = $isPrev
 			? '<svg class="govuk-pagination__icon govuk-pagination__icon--prev" xmlns="http://www.w3.org/2000/svg" height="13" width="15" aria-hidden="true" focusable="false" viewBox="0 0 15 13"><path d="m6.5938-0.0078125-6.7266 6.7266 6.7441 6.4062 1.377-1.449-4.1856-3.9768h12.896v-2h-12.984l4.2931-4.293-1.414-1.414z"></path></svg>'
@@ -21,7 +28,7 @@ function renderPaginationLink($post, $isPrev = true)
 	ob_start();
 	?>
     <div class="<?= esc_attr($class) ?>">
-        <?php if ($url): ?>
+        <?php if ($url !== ''): ?>
             <a class="govuk-link govuk-pagination__link" href="<?= esc_url($url) ?>" rel="<?= esc_attr($rel) ?>">
                 <?= $svg ?>
                 <span class="govuk-pagination__link-title"><?= esc_html($titleText) ?></span>
@@ -38,35 +45,65 @@ function renderPaginationLink($post, $isPrev = true)
         <?php endif; ?>
     </div>
     <?php
-	return ob_get_clean();
+	return (string) ob_get_clean();
 }
 
-function renderPaginationBlock()
+/**
+ * @return string
+ */
+function renderPaginationBlock(): string
 {
 	$post_id = get_the_ID();
+	if ($post_id === false || $post_id === 0) {
+		return '';
+	}
+
 	$post = get_post($post_id);
+	if (!$post instanceof WP_Post) {
+		return '';
+	}
 
 	$parent_id = $post->post_parent ?: $post_id;
 	$parent_page = get_post($parent_id);
-
-	$children = get_pages([
-			'parent' => $parent_id,
-			'sort_column' => 'menu_order',
-			'sort_order' => 'asc',
-	]);
-
-	$pages = [$parent_page];
-	foreach ($children as $child) {
-		if ($child->ID !== $parent_id) {
-			$pages[] = $child;
-		}
+	if (!$parent_page instanceof WP_Post) {
+		return '';
 	}
 
-	$pages_ids = wp_list_pluck($pages, 'ID');
-	$index = array_search($post_id, $pages_ids);
+	$children_raw = get_pages([
+		'parent' => $parent_id,
+		'sort_column' => 'menu_order',
+		'sort_order' => 'asc',
+		'post_status' => 'publish',
+	]);
 
-	$prev = ($index > 0) ? $pages[$index - 1] : null;
-	$next = ($index < count($pages) - 1) ? $pages[$index + 1] : null;
+	/** @var WP_Post[] $children */
+	$children = is_array($children_raw) ? $children_raw : [];
+
+	/** @var WP_Post[] $pages */
+	$pages = [$parent_page];
+
+	foreach ($children as $child) {
+		$pages[] = $child;
+	}
+
+	/** @var int[] $pages_ids */
+	$pages_ids = array_map(fn (WP_Post $p): int => $p->ID, $pages);
+
+	$index = array_search($post_id, $pages_ids, true);
+
+	/** @var WP_Post|null $prev */
+	$prev = null;
+	/** @var WP_Post|null $next */
+	$next = null;
+
+	if (is_int($index)) {
+		if ($index > 0) {
+			$prev = $pages[$index - 1];
+		}
+		if ($index < count($pages) - 1) {
+			$next = $pages[$index + 1];
+		}
+	}
 
 	ob_start();
 	?>
@@ -75,5 +112,5 @@ function renderPaginationBlock()
         <?= renderPaginationLink($next, false) ?>
     </nav>
     <?php
-	return ob_get_clean();
+	return (string) ob_get_clean();
 }
--- app/Blocks/Pagination/build/index.asset.php
+++ app/Blocks/Pagination/build/index.asset.php
@@ -1,1 +1,3 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'b1dab04ce5298b2ba331');
+<?php
+
+return ['dependencies' => ['react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'], 'version' => 'b1dab04ce5298b2ba331'];
