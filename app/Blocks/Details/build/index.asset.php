afb03cad14201f54346cc5358260571887857d9b
Sim Brody <sim@dxw.com>
2026-04-09 14:32:45 +0100

    PHP cs linting

--- app/Blocks/Details/build/index.asset.php
+++ app/Blocks/Details/build/index.asset.php
@@ -1,1 +1,3 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'e9860c28b8c0b778bae6');
+<?php
+
+return ['dependencies' => ['react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'], 'version' => 'e9860c28b8c0b778bae6'];
