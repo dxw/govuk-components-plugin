d8f3d3c94a45006fbb126d222812a1e69e788fec
Sim Brody <sim@dxw.com>
2026-04-09 14:30:58 +0100

    build: Rebuild Details block with render callback
    Compile updated block.json and render.php to build directory.
    Generates render.php in build for WordPress block registration.

--- app/Blocks/Details/build/index.asset.php
+++ app/Blocks/Details/build/index.asset.php
@@ -1,3 +1,1 @@
-<?php
-
-return ['dependencies' => ['react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'], 'version' => '69ea3a1876d96e08a8ff'];
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'e9860c28b8c0b778bae6');
