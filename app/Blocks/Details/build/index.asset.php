697d148f7991096882cf30b213085e6fa400d6de
Sim Brody <sim@dxw.com>
2026-04-09 11:59:49 +0100

    build: Rebuild Details block after refactor
    Regenerate compiled block assets after extracting Renderer class.
    No functional changes - just updated build artifacts.

--- app/Blocks/Details/build/index.asset.php
+++ app/Blocks/Details/build/index.asset.php
@@ -1,3 +1,3 @@
 <?php
 
-return ['dependencies' => ['react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'], 'version' => 'f656830d89cae6c965f2'];
+return ['dependencies' => ['react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'], 'version' => '69ea3a1876d96e08a8ff'];
