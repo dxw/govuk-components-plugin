d16ff737da4630941ff61f3ef1fff99fd451a7c0
william-man <william.man@dxw.com>
2026-04-09 15:25:26 +0100

    Build src files

--- app/Blocks/InsetText/build/index.asset.php
+++ app/Blocks/InsetText/build/index.asset.php
@@ -1,3 +1,1 @@
-<?php
-
-return ['dependencies' => ['react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'], 'version' => 'edb8efcf04bf22d7f263'];
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'edb8efcf04bf22d7f263');
--- app/Blocks/InsetText/build/render.php
+++ app/Blocks/InsetText/build/render.php
@@ -2,5 +2,5 @@
 /** @var string $content */
 ?>
 <div <?php echo get_block_wrapper_attributes(['class' => 'govuk-inset-text']); ?>>
-    <?php echo $content ?>
+    <?php echo wp_kses_post($content) ?>
 </div>
