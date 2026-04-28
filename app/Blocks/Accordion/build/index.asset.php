6f4246c13027952b0a7ae4afd546d2f72f2bb335
Sim Brody <sim@dxw.com>
2026-04-28 12:17:02 +0100

    PHP cs lint

--- app/Blocks/Accordion/build/index.asset.php
+++ app/Blocks/Accordion/build/index.asset.php
@@ -1,1 +1,3 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '95f0fef89753c8d46108');
+<?php
+
+return ['dependencies' => ['react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'], 'version' => '95f0fef89753c8d46108'];
