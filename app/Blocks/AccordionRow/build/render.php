c23363e1a9a16980531ddd0310e994f268acf13b
Sim Brody <sim@dxw.com>
2026-04-28 14:51:36 +0100

    Add render file as part of dynamic refactor
    Part of process of turning block from static to dynamic and using
    PHP to render front end

---
+++ app/Blocks/AccordionRow/build/render.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @var string $content
+ * @var array $attributes
+ */
+
+// Check if this is an old block that already has the accordion row markup saved
+if (strpos($content, '<div class="govuk-accordion__section"') !== false) {
+	// Old block format - content already contains full markup, return as-is
+	echo wp_kses_post($content);
+	return;
+}
+
+$header = $attributes['header'] ?? '';
+$index = $attributes['index'] ?? 0;
+$wrapper_attributes = get_block_wrapper_attributes([
+	'class' => 'govuk-accordion__section',
+]);
+
+?>
+<div <?php echo $wrapper_attributes; ?>>
+	<div class="govuk-accordion__section-header">
+		<h2 class="govuk-accordion__section-heading">
+			<span class="govuk-accordion__section-button" id="accordion-default-heading-<?php echo $index; ?>">
+				<?php echo wp_kses_post($header); ?>
+			</span>
+		</h2>
+	</div>
+	<div class="govuk-accordion__section-content" id="accordion-default-content-<?php echo $index; ?>">
+		<?php echo wp_kses_post($content); ?>
+	</div>
+</div>
