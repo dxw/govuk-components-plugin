d78bc8ed5be3bc7884cc169b590e05bdfe861855
Sim Brody <sim@dxw.com>
2026-05-05 12:31:11 +0100

    Enable UniqueID attribute to be passed from parent
    This will allow the ID to be given to child accordion panels,
    so that the open all button can be used to open the panels of a
    single accordion only. This is handled by govuk js but is needed
    for non-govuk implementations

--- app/Blocks/AccordionRow/build/index.asset.php
+++ app/Blocks/AccordionRow/build/index.asset.php
@@ -1,3 +1,1 @@
-<?php
-
-return ['dependencies' => ['react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-i18n'], 'version' => 'e64ccbb5e8dec2496e15'];
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-i18n'), 'version' => 'edc286698a63f5b25ea2');
