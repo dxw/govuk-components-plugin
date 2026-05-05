5521bbb91cc7dae0313e413607f0be0af6c0b407
Sim Brody <sim@dxw.com>
2026-05-05 13:00:52 +0100

    PHP lint

--- app/Blocks/Accordion/build/index.asset.php
+++ app/Blocks/Accordion/build/index.asset.php
@@ -1,1 +1,3 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '4cce35b204159f3f93b7');
+<?php
+
+return ['dependencies' => ['react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'], 'version' => '4cce35b204159f3f93b7'];
--- app/Blocks/AccordionRow/build/index.asset.php
+++ app/Blocks/AccordionRow/build/index.asset.php
@@ -1,1 +1,3 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-i18n'), 'version' => 'edc286698a63f5b25ea2');
+<?php
+
+return ['dependencies' => ['react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-i18n'], 'version' => 'edc286698a63f5b25ea2'];
